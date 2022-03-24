fdcd19a3e5ebdc8cc941f25c8cf03bd3a414dcc6
Tommy Chen <tommy.cxy@growthops.asia>
2022-03-24 17:44:52 +0800

    Implemented Header component and page styling ✨

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue'
+import $ from 'jquery'
+
+const toggleDarkMode = () => {
+  $(".dark-mode-toggle").find('img').toggleClass("hidden");
+  $(".app").toggleClass("dark-theme");
+};
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <h1 class="text-white">TODO</h1>
+    </div>
+    <button
+      @click="toggleDarkMode()"
+      class="dark-mode-toggle">
+      <img src="../assets/icon-moon.svg" alt="moon icon" />
+      <img src="../assets/icon-sun.svg" alt="sun icon" class="hidden" />
+    </button>
+  </header>
+</template>
+
+<style>
+
+</style>
